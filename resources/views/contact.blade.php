b350406ea4b6fc55092054c1806e26e16ebf40c0
EijiroNotME <jusphervergara09@gmail.com>
2024-11-19 15:22:51 +0800

    third-day

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Document</title>
-</head>
-<body>
-    <h1 class="text-2xl text-blue-500">Hello from contact page</h1>
-</body>
-</html>
+<x-layout>
+    <h1 class="text-2xl text-red-500">Hello from contact page</h1>
+</x-layout>
