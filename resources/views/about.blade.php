1f2080aea6050b84360c762e82c9a96ba5e03885
EijiroNotME <jusphervergara09@gmail.com>
2024-11-20 15:26:53 +0800

    4th-day

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>About</x-slot:heading>
     <h1 class="text-2xl text-blue-500">Hello from about page</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>Contact</x-slot:heading>
     <h1 class="text-2xl text-red-500">Hello from contact page</h1>
 </x-layout>
