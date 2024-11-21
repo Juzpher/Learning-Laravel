bf2c2c1f9776f4b0bc5c407906ee9422845f4780
EijiroNotME <jusphervergara09@gmail.com>
2024-11-22 06:45:36 +0800

    7th-day

---
+++ app/Models/Job.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Arr;
+
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => "$50,000",
+            ],
+            [
+                'id' => 2,
+                'title' => 'Manager',
+                'salary' => "$40,000",
+            ],
+            [
+                'id' => 3,
+                'title' => 'Engineer',
+                'salary' => "$30,000",
+            ],
+            [
+                'id' => 4,
+                'title' => 'Intern',
+                'salary' => "$10,000",
+            ],
+        ];
+    }
+
+    public static function find($id): array
+    {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+        if (!$job) {
+            abort(404);
+        }
+        return $job;
+    }
+}
