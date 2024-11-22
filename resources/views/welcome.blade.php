abe5983534244877b96285a7fd3da8026ba854bf
EijiroNotME <jusphervergara09@gmail.com>
2024-11-22 08:13:39 +0800

    8th-day

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-   <x-slot:heading>Home</x-slot:heading>
+    <x-slot:heading>Home</x-slot:heading>
+    <?php
+    if (DB::connection()->getDatabaseName()) {
+        echo 'Connected to DB ' . DB::connection()->getDatabaseName();
+    }
+    
+    ?>
 </x-layout>
